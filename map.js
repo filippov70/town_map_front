62ec8e22743221698be594cf293b97e5ad555c6d
Владислав <filippov70@gmail.com>
2020-02-04 19:18:13 +0700

    Update map.js
    связывание данных с popup

--- map.js
+++ map.js
@@ -1,4 +1,12 @@
 function loadMap() {
+  function onEachFeature(feature, layer) {
+    // does this feature have a property named 'name'?
+    if (feature.properties && feature.properties.name) {
+      layer.bindPopup(feature.properties.name + '\nучеников ' +
+        feature.properties.p);
+        console.log(feature.properties.name);
+    }
+  }
   var map = L.map('map').setView([54.93, 83.18], 12);
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,5 +14,7 @@ function loadMap() {
       attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
-  L.geoJSON(geojsonFeature).addTo(map);
+  L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+  }).addTo(map);
 }
