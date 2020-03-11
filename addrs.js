dea095e658288ffac807826953c28fb566d4e31d
filippov70 <filippov70@gmail.com>
2020-03-11 15:05:03 +0700

    Добавлен поиск и отображение найденного

---
+++ addrs.js
@@ -0,0 +1,14 @@
+var address = [
+    {value:"1", data: {school: "1", vote: "1"}},
+    {value:"2", data: {school: "1", vote: "1"}},
+    {value:"3", data: {school: "1", vote: "1"}},
+    {value:"4", data: {school: "1", vote: "1"}},
+    {value:"5", data: {school: "1", vote: "1"}},
+    {value:"6", data: {school: "1", vote: "1"}},
+    {value:"Садовая ул, 1", data: {school: "1", vote: "1"}},
+
+    {value:"АБК", data: {school: "2", vote: "1"}},
+    {value:"21", data: {school: "2", vote: "1"}},
+    {value:"22", data: {school: "2", vote: "1"}},
+    {value:"23", data: {school: "2", vote: "1"}}
+];
